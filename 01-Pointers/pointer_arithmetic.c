504a5a1dba7955a025ed1163b154523e46ef191e
Tan5dev <tanmaypathe4@gmail.com>
2026-06-22 19:44:54 +0530

    Create pointer_arithmetic.c
    Add pointer arithmetic example

---
+++ 01-Pointers/pointer_arithmetic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int *ptr = arr;
+
+    printf("Array elements using pointer arithmetic:\n");
+
+    for(int i = 0; i < 5; i++) {
+        printf("%d ", *(ptr + i));
+    }
+
+    printf("\n");
+
+    return 0;
+}
