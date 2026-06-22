88e11f96f66a3eed89d7a7fb63e4387a26187ef4
Tan5dev <tanmaypathe4@gmail.com>
2026-06-22 19:48:00 +0530

    Create 02-Memory-Management/malloc_example.c
    Add malloc example

---
+++ 02-Memory-Management/malloc_example.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n = 5;
+
+    int *arr = (int *)malloc(n * sizeof(int));
+
+    if (arr == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
+    for(int i = 0; i < n; i++) {
+        arr[i] = (i + 1) * 10;
+    }
+
+    printf("Array elements:\n");
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    free(arr);
+
+    return 0;
+}
